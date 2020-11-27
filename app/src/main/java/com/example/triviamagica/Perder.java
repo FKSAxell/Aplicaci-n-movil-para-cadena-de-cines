8509d1384d70b434a8d7bc1ddf85fffe9ea89b92
FKSAxell <kevin.concha.regatto@outlook.com>
2020-11-27 12:47:09 -0500

    pantalla de peder y cambio de colores

---
+++ app/src/main/java/com/example/triviamagica/Perder.java
@@ -0,0 +1,14 @@
+package com.example.triviamagica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Perder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perder);
+    }
+}
